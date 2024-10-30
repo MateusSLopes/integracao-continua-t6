2ec8ed6b85c99462dc5e2767a932156ec999d46f
Mateus Lopes <mateus_tiwu@hotmail.com>
2024-10-30 18:47:51 -0500

    feat: teste Deploy

--- sgcmapi/src/test/java/br/ufac/sgcmapi/PacienteTest.java
+++ sgcmapi/src/test/java/br/ufac/sgcmapi/PacienteTest.java
@@ -33,8 +33,8 @@ public class PacienteTest {
 	private final MockMvc mockMvc;
 
 	@Autowired
-	PacienteTest(MockMvc mockMvc) {
-		this.mockMvc = mockMvc;
+	PacienteTest(MockMvc mock) {
+		this.mockMvc = mock;
 	}
 
     @Test
